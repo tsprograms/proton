c0ce98723f1e300550ce8bc2ed2cf7f04cfb84cb
TS Programs <tsgithub@gmail.com>
2016-05-13 22:14:47 +0000

    Add basic files for Electron/Node to use

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var app = require('app');
+var BrowserWindow = require('browser-window');
+
+var mainWindow = null;
+
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width:  800
+    });
+    mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+});
