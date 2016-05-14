0450fe275b97425e35eff03eb051e2b926083ab7
tsprograms <tsprograms@users.noreply.github.com>
2016-05-14 13:36:21 -0400

    Add basic Window constructor to js/windows.js

--- js/windows.js
+++ js/windows.js
@@ -3,3 +3,18 @@ js/windows.js
 Created on 5/14/16 by TSPrograms.
 Copyright © 2016 TSPrograms.
 */
+
+var electron = require('electron');
+var BrowserWindow = require('browser-window');
+
+var tabs = require('./tabs');
+var prefs = require('./prefs');
+
+exports.Window = function(initialURL, options) {
+  this.browserWindow = new BrowserWindow(options);
+  this.tabs = [
+    new tabs.Tab(initialURL)
+  ];
+
+  this.browserWindow.loadUrl('file://' + __dirname + '/app/index.html');
+};
